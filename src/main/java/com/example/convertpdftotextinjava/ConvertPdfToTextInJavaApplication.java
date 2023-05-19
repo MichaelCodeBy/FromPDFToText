73abe56a06b4e9af0781f7034e4d1f6a740a3b98
MichaelCodeBy <93823188+MichaelCodeBy@users.noreply.github.com>
2023-05-19 19:20:14 +0300

    start this work

---
+++ src/main/java/com/example/convertpdftotextinjava/ConvertPdfToTextInJavaApplication.java
@@ -0,0 +1,60 @@
+package com.example.convertpdftotextinjava;
+
+import com.aspose.pdf.Document;
+import com.aspose.pdf.TextAbsorber;
+import com.aspose.pdf.TextExtractionOptions;
+import com.aspose.pdf.internal.html.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+@SpringBootApplication
+public class ConvertPdfToTextInJavaApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(ConvertPdfToTextInJavaApplication.class, args);
+
+
+        // main method to convert a PDF document to Text file
+        // Instantiate the license to avoid trial limitations while converting the PDF to a text file
+        License asposePdfLicenseText = new License();
+        asposePdfLicenseText.setLicense("Aspose.pdf.lic");
+
+
+        // Load the source PDF file that is to be converted to Text file
+        Document convertPDFDocumentToText = new Document("input.pdf");
+
+
+        // Instantiate a TextAbsorber class object for converting PDF to Text
+        TextAbsorber textAbsorber = new TextAbsorber(new TextExtractionOptions(TextExtractionOptions.TextFormattingMode.Pure));
+
+
+        // Call the Accept method exposed by the TextAbsorber class
+        convertPDFDocumentToText.getPages().accept(textAbsorber);
+
+
+        // Read the text as string
+        String ExtractedText = textAbsorber.getText();
+
+
+        // Create the BufferedWriter object to open the file
+        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("SampleOutput.txt")));
+
+        // Write extracted contents to the file
+        writer.write(ExtractedText);
+
+        // Close writer
+        writer.close();
+
+
+        System.out.println("Done");
+    }
+
+   // public static void main(String[] args) {
+    }
+
+
---
+++ src/test/java/com/example/convertpdftotextinjava/ConvertPdfToTextInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertpdftotextinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertPdfToTextInJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
